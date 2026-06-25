8e56b4b557b5d66f50ca0e4ebe22665e1ffed9cd
Yves teuguia <teuguiayves33@gmail.com>
2026-06-25 20:53:28 +0100

    Seed admin account from environment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Compte administrateur principal; le mot de passe reste dans l'environnement, pas dans Git.
+        if ($adminPassword = env('ADMIN_PASSWORD')) {
+            User::query()->updateOrCreate(
+                ['name' => env('ADMIN_NAME', 'MINESEC1')],
+                [
+                    'email' => env('ADMIN_EMAIL', 'admin@universitykey.cm'),
+                    'password' => Hash::make($adminPassword),
+                    'role' => 'admin',
+                    'statut' => 'actif',
+                    'verification_requise' => false,
+                    'email_verified_at' => now(),
+                    'telephone_verified_at' => now(),
+                    'telephone' => env('ADMIN_PHONE', '690232871'),
+                    'langue_preferee' => 'fr',
+                ]
+            );
+        }
+
         // Textes legaux publics necessaires aux modales conditions/confidentialite.
         $this->call(RegleSeeder::class);
         // Test general d'orientation: 20 questions et poids par grandes filieres.
